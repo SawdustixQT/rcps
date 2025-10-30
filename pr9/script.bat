d6615865a2a2a4ce3cad39c168a2aa5eb050ab73
Chi <p1tu4@yandex.ru>
2025-10-30 21:59:20 +0300

    Добавлена 9 задача

---
+++ pr9/script.bat
@@ -0,0 +1,3 @@
+gcc -I"lua-5.4.8/src" script.c -L"lua-5.4.8/src" -llua -o test
+.\test.exe
+pause
---
+++ pr9/script.lua
@@ -0,0 +1,4 @@
+function discriminante(a, b, c):
+	print("Hello from LUA");
+	return b * b - (4 * a * c)
+end 
